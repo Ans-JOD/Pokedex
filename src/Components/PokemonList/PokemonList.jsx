834f3f4272899892327ff7e1fcf1af27a5b68480
Anshul <anshulb@chromeinfotech.com>
2025-05-05 19:01:56 +0530

    Added prev and next buttons and its funcitonalities

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -6,10 +6,18 @@ import Pokemon from "../Pokemon/Pokemon";
 const PokemonList = () => {
     const [loading, setLoading] = useState(true);
     const [pokemonList, SetPokemonList] = useState([]);
+    const [pokedexURL, setPodedexURL] = useState('https://pokeapi.co/api/v2/pokemon');
+    const [prevURL, setPrevULR] = useState('');
+    const [nextURL, setNextURL] = useState('');
 
     async function PokemonList(){
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        setLoading(true)
+        const response = await axios.get(pokedexURL);
         const pokemonResult = response.data.results;
+        setNextURL(response.data.next);
+        console.log(response.data.next);
+        setPrevULR(response.data.previous);  
+        console.log(response.data.previous);           
         const pokemonResultPromise = pokemonResult.map((pokemon)=> axios.get(pokemon.url));
         const pokemonData = await Promise.all(pokemonResultPromise);
         const res = Array.isArray(pokemonData) ? pokemonData.map((pokeData) => {
@@ -29,13 +37,18 @@ const PokemonList = () => {
     useEffect(() => {
         PokemonList();
         setLoading(false);
-    }, []);
+    }, [pokedexURL, nextURL, prevURL]);
 
 
     return (
         <div className="pokemon-list">
 
-            <span className="list-heading">List of pokemons</span>
+            <div className="heading-with-button">
+                <button className="btn" disabled={prevURL == null} onClick={() => setPodedexURL(prevURL)}>Prev</button>
+                <span className="list-heading">List of pokemons</span> 
+                <button className="btn" disabled={nextURL == null} onClick={() => setPodedexURL(nextURL)}>Next</button>
+            </div>
+            
             <div className="pokemon-wrapper">
                 
             {loading ? 'Loading...' : 
